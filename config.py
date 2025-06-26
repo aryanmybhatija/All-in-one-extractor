9a208123b1f5b96d5a6e828ccefb783d92ad2011
aryanmybhatija <sonutomarlucky@gmail.com>
2025-06-26 11:13:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,16 +9,16 @@ API_HASH = os.environ.get("API_HASH", "")
 # ----------------D--------------------------------
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("@All_Extractorbot")
+BOT_USERNAME = os.environ.get("@Image_Uploader1_bot")
 # ------------------X------------------------------
 OWNER_ID = int(os.environ.get("OWNER_ID", "6890400066"))
 # ------------------X------------------------------
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6890400066 5744263553").split()))
 # ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002594448328"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002527188402"))
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://user:user1@cluster0.sp0pitj.mongodb.net/")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002561868621"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002520864904"))
 
